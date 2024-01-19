8285353002d4f0e1072647ab8a1c2b8927cdec6a
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-01-19 11:26:23 +0100

    add method fill for filling db and in store function

--- app/Http/Controllers/admin/ComicController.php
+++ app/Http/Controllers/admin/ComicController.php
@@ -33,13 +33,14 @@ class ComicController extends Controller
         $form_data = $request->all();
 
         $new_comic = new Comic();
-        $new_comic->title = $form_data['title'];
-        $new_comic->description = $form_data['description'];
-        $new_comic->thumb = $form_data['thumb'];
-        $new_comic->price = $form_data['price'];
-        $new_comic->series = $form_data['series'];
-        $new_comic->sale_date = $form_data['sale_date'];
-        $new_comic->type = $form_data['type'];
+        // $new_comic->title = $form_data['title'];
+        // $new_comic->description = $form_data['description'];
+        // $new_comic->thumb = $form_data['thumb'];
+        // $new_comic->price = $form_data['price'];
+        // $new_comic->series = $form_data['series'];
+        // $new_comic->sale_date = $form_data['sale_date'];
+        // $new_comic->type = $form_data['type'];
+        $new_comic->fill($form_data);
 
         $new_comic->save();
 
